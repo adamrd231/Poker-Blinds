5091f77c9f73e8d7510a2a5249ab56fd2c65f7af
adamrd231 <adam.rdconcepts@gmail.com>
2021-07-17 13:15:26 -0400

    Initial Commit

---
+++ PokerBlinds/PokerBlindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerBlindsApp.swift
+//  PokerBlinds
+//
+//  Created by Adam Reed on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct PokerBlindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
