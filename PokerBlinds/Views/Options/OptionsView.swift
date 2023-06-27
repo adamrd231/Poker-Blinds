7e3a7dc71e81981405bee12c98b3599fac369df4
Adam Reed <adam.rdconcepts@gmail.com>
2023-06-27 14:56:25 -0400

    Use list component instead of vstacks

--- PokerBlinds/Views/Options/OptionsView.swift
+++ PokerBlinds/Views/Options/OptionsView.swift
@@ -49,11 +49,64 @@ struct OptionsView: View {
     @State var isPortrait: Bool = true
     
     var body: some View {
-        wrappedView
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                guard let scene = UIApplication.shared.windows.first?.windowScene else { return }
-                self.isPortrait = scene.interfaceOrientation.isPortrait
-            }
+        VStack {
+            List {
+                Section(header: Text("Game settings")) {
+                    HStack {
+                        Text("Time in round")
+                        Spacer()
+                        HStack(spacing: .zero) {
+                            // Minutes
+                            Text(vm.timerInfo.currentMinutes.description)
+                           // Seconds
+                            Text(":")
+                            Text(vm.timerInfo.currentSeconds == 0 ? "00" : vm.timerInfo.currentSeconds.description)
+                        }
+                        Stepper("", value: $vm.timerInfo.currentTime, in: 10...1000, step: 10)
+                            .fixedSize()
+                    }
+                    
+                    OptionRowBlindView(text: "Starting Blinds", blind: $vm.blindInfo.startingSmallBlind)
+                    OptionRowView(text: "Raise blinds by", firstValue: $vm.blindInfo.amountToRaiseBlinds)
+                    OptionRowBlindView(text: "Blind limit", blind: $vm.blindInfo.blindLimit)
+                }
+
+                Section(header: Text("Sound")) {
+                    HStack {
+                        Text("End of Round")
+                        Spacer()
+                        Picker("", selection: $vm.currentSound) {
+                            ForEach(SoundManager.instance.allSounds.indices, id: \.self) { index in
+                                Text(SoundManager.instance.allSounds[index].rawValue)
+                            }
+                        }
+                        .onChange(of: vm.currentSound, perform: { newValue in
+                            SoundManager.instance.playSound(sound: SoundManager.instance.allSounds[vm.currentSound])
+                        })
+                    }
+                }
+                
+                Section(header: Text("Blind table")) {
+                        ForEach(Array(zip(vm.blindsArray.indices, vm.blindsArray)), id: \.0) { index, level in
+                            HStack {
+                                Text("Level \(index + 1)")
+                                Rectangle()
+                                    .foregroundColor(Color.theme.mainButton.opacity(0.5))
+                                    .frame(height: 1)
+                                    .padding(.horizontal)
+                                
+                                Text(level.smallBlind.description)
+                                Text("|")
+                                Text(level.bigBlind.description)
+                            }
+                        }
+                    }
+                }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+            guard let scene = UIApplication.shared.windows.first?.windowScene else { return }
+            self.isPortrait = scene.interfaceOrientation.isPortrait
+        }
     }
 }
 
