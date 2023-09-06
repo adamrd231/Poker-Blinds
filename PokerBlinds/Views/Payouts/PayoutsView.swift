4bead706daa903144a50a2e9c9a51f9a65d1d6d9
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-06 18:12:47 -0400

    High hand should be an option, not baked in. Also allow to update buy in fee, how much high hand should be

--- PokerBlinds/Views/Payouts/PayoutsView.swift
+++ PokerBlinds/Views/Payouts/PayoutsView.swift
@@ -7,20 +7,29 @@
 
 import SwiftUI
 
+let numberFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    formatter.zeroSymbol  = ""
+    return formatter
+}()
+
 
 struct PayoutsView: View {
     
     @StateObject var payoutsVM = PayoutsViewModel()
     @State var twoPlayerPayoutTotal: String = ""
+    @State var twoPlayerPayout = 0
     @State var playerOnePayout: Int = 0
     @State var playerTwoPayout: Int = 0
     
     @State var playerArray = ["1st place", "2nd place"]
     @State var selectedPlayer = "1st place"
+    @State var isUsingHighHand: Bool = false
     
     var body: some View {
         List {
-            Section(header: Text("Payouts calc")) {
+            Section(header: Text("Options")) {
                 HStack {
                     Stepper("Players \(payoutsVM.players.count)",
                             onIncrement: { payoutsVM.addPlayer() },
@@ -34,9 +43,19 @@ struct PayoutsView: View {
                         Stepper("", value: $payoutsVM.startingStack, in: 1_000...100_000, step: 1_000)
                     }
                 }
+                HStack {
+                    Text("High hand")
+                    Toggle("", isOn: $isUsingHighHand)
+                }
+            }
+            Section(header: Text("Payouts calc")) {
+               
                 PayoutRowView(place: "Total money", payout: payoutsVM.getTotalMoney())
                 PayoutRowView(place: "Prize pool", payout: payoutsVM.getTotalPrizeMoney())
-                PayoutRowView(place: "High hand", payout: payoutsVM.highHandPrize)
+                if isUsingHighHand {
+                    PayoutRowView(place: "High hand", payout: payoutsVM.highHandPrize)
+                }
+               
                 PayoutRowView(place: "1st", payout: payoutsVM.firstPlacePrize)
                 PayoutRowView(place: "2nd", payout: payoutsVM.secondPlacePrize)
                 if payoutsVM.thirdPlacePrize != 0 {
@@ -47,7 +66,7 @@ struct PayoutsView: View {
             Section(header: Text("End game with two players")) {
                 Text("Enter total for either player")
                 
-                TextField("Enter player chip total", text: $twoPlayerPayoutTotal)
+                TextField("Enter either player chip total", value: $twoPlayerPayout, formatter: numberFormatter)
                 Button("Calculate payout") {
   
                     let removeCommas = twoPlayerPayoutTotal.replacingOccurrences(of: ",", with: "")
@@ -110,12 +129,12 @@ struct PayoutRowView: View {
     var body: some View {
         HStack {
             Text(place)
-                .font(.caption)
             Spacer()
             HStack(spacing: 0) {
                 Text("$")
                 Text(payout, format: .number)
             }
+            .bold()
         }
     }
 }
