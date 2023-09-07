5e43a8ec5742a1a0774773f719b7b1fc9c0c6b71
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-07 14:56:29 -0400

    Move extension to its own file

--- PokerBlinds/Views/HomeScreen/PokerBlindsView.swift
+++ PokerBlinds/Views/HomeScreen/PokerBlindsView.swift
@@ -1,26 +1,5 @@
 import SwiftUI
 
-// Our custom view modifier to track rotation and
-// call our action
-struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
-
-    func body(content: Content) -> some View {
-        content
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-                print("Current orientation \(UIDevice.current.orientation)")
-            }
-    }
-}
-
-// A View wrapper to make the modifier easier to use
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(DeviceRotationViewModifier(action: action))
-    }
-}
-
 struct PokerBlindsView: View {
     // Data objects and services
     @EnvironmentObject var vm: ViewModel
