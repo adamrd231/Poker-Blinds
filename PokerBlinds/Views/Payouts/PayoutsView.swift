f687ba49b63062468c794c755ea976ecd2e62bc8
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-10 11:20:43 -0400

    Add buttons to the keyboard when filling out the textfield on payouts screen.

--- PokerBlinds/Views/Payouts/PayoutsView.swift
+++ PokerBlinds/Views/Payouts/PayoutsView.swift
@@ -11,9 +11,15 @@ struct PayoutsView: View {
     
     @ObservedObject var storeManager: StoreManager
     @StateObject var payoutsVM = PayoutsViewModel()
+    @FocusState private var textFieldIsFocused: Bool
 
     @State var playerArray = ["1st place", "2nd place"]
     @State var selectedPlayer = "1st place"
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
 
 
     var body: some View {
@@ -21,20 +27,38 @@ struct PayoutsView: View {
             Section(header: Text("Options")) {
                 HStack {
                     Stepper("Players \(payoutsVM.players.count)",
-                            onIncrement: { payoutsVM.addPlayer() },
-                            onDecrement: { payoutsVM.removePlayer() })
+                        onIncrement: {
+                            simpleSuccess()
+                            payoutsVM.addPlayer()
+                        }, onDecrement: {
+                            simpleSuccess()
+                            payoutsVM.removePlayer()
+                        }
+                    )
+  
+       
                 }
                 HStack {
                     Stepper("Buy-in $\(payoutsVM.buyIn)", value: $payoutsVM.buyIn, in: 0...1000, step: 1)
+                        .onChange(of: payoutsVM.buyIn) { newValue in
+                            simpleSuccess()
+                        }
                 }
                 
                 VStack {
                     HStack {
                         Text("High hand")
                         Toggle("", isOn: $payoutsVM.isUsingHighHand)
+                            .onChange(of: payoutsVM.isUsingHighHand) { newValue in
+                                simpleSuccess()
+                            }
+            
                     }
                     if payoutsVM.isUsingHighHand {
                         Stepper("Cost (less from buy in) $\(payoutsVM.highHandContribution)", value: $payoutsVM.highHandContribution, in: 1...100, step: 1)
+                            .onChange(of: payoutsVM.highHandContribution) { newValue in
+                                simpleSuccess()
+                            }
                     }
                 }
             }
@@ -62,6 +86,10 @@ struct PayoutsView: View {
                         Text("$")
                         Text(payoutsVM.startingStack, format: .number)
                         Stepper("", value: $payoutsVM.startingStack, in: 1_000...100_000, step: 1_000)
+                            .onChange(of: payoutsVM.startingStack) { newValue in
+                                simpleSuccess()
+                            }
+                        
                     }
                 }
                 Text("Enter final total for either player")
@@ -70,9 +98,24 @@ struct PayoutsView: View {
                           value: $payoutsVM.splitPotPayout,
                           formatter: numberFormatter
                 )
+                .focused($textFieldIsFocused)
                 .keyboardType(.numberPad)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button("Dismiss") {
+                            simpleSuccess()
+                            textFieldIsFocused = false
+                        }
+                        Button("Calculate") {
+                            simpleSuccess()
+                            payoutsVM.calculatePayout()
+                            textFieldIsFocused = false
+                        }
+                    }
+                }
    
                 Button("Calculate payout") {
+                    simpleSuccess()
                     payoutsVM.calculatePayout()
                 }
                 
