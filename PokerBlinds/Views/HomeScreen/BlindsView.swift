8b204ca09b546bfaa5d463cb5f40442685f5a2a3
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-11 11:50:06 -0400

    Fix bug where timer crashes when current level is outside of the blind levels

--- PokerBlinds/Views/HomeScreen/BlindsView.swift
+++ PokerBlinds/Views/HomeScreen/BlindsView.swift
@@ -1,5 +1,25 @@
 import SwiftUI
 
+struct BlindView: View {
+    
+    let blind: Int
+    let fontSize: Double
+    let isSmallBlind: Bool
+    
+    var body: some View {
+        VStack(spacing: .zero) {
+            if isSmallBlind {
+                SmallBlindHeader()
+            } else {
+                BigBlindHeader()
+            }
+            
+            Text("\(blind)")
+                .font(Font.system(size: UIFontMetrics.default.scaledValue(for: fontSize), weight: .heavy, design: .rounded))
+        }
+    }
+}
+
 struct BlindsView: View {
     let fontSize: Double
     let blindLevels: [BlindLevel]
@@ -9,29 +29,20 @@ struct BlindsView: View {
     
     var body: some View {
         VStack(alignment: .center) {
-            if let blinds = blindLevels[currentLevel] {
-                VStack(spacing: 10) {
-                    VStack(spacing: .zero) {
-                         SmallBlindHeader()
-                         Text("\(blinds.smallBlind)")
-                             .font(Font.system(size: UIFontMetrics.default.scaledValue(for: fontSize), weight: .heavy, design: .rounded))
-                     }
-                     VStack(spacing: .zero) {
-                         BigBlindHeader()
-                         Text("\(blinds.bigBlind)")
-                             .font(Font.system(size: UIFontMetrics.default.scaledValue(for: fontSize), weight: .heavy, design: .rounded))
-                     }
+            if currentLevel >= blindLevels.count  {
+                if let last = blindLevels.last {
+                    VStack(spacing: 15) {
+                        BlindView(blind: last.smallBlind, fontSize: fontSize, isSmallBlind: true)
+                        BlindView(blind: last.bigBlind, fontSize: fontSize, isSmallBlind: false)
+                    }
                 }
-            } else if let lastBlind = blindLevels.last {
-                VStack {
-                    Text("small")
-                    Text("\(lastBlind.smallBlind)")
-                        .font(Font.system(size: UIFontMetrics.default.scaledValue(for: fontSize), weight: .heavy, design: .rounded))
-                    Text("big")
-                    Text("\(lastBlind.bigBlind)")
-                        .font(Font.system(size: UIFontMetrics.default.scaledValue(for: fontSize), weight: .heavy, design: .rounded))
+            } else if let blinds = blindLevels[currentLevel] {
+                VStack(spacing: 15) {
+                    BlindView(blind: blinds.smallBlind, fontSize: fontSize, isSmallBlind: true)
+                    BlindView(blind: blinds.bigBlind, fontSize: fontSize, isSmallBlind: false)
                 }
             }
+            
         }
         .padding(.horizontal)
     }
