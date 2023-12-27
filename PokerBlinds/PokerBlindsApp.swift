f52c46a77a6c31b2a0ab19d5ef373e27bd4a42b6
Adam Reed <adam.rdconcepts@gmail.com>
2023-12-27 12:43:45 -0500

    Fix content obscuring google admob banner

--- PokerBlinds/PokerBlindsApp.swift
+++ PokerBlinds/PokerBlindsApp.swift
@@ -4,14 +4,12 @@
 //
 //  Created by Adam Reed on 7/17/21.
 //
-
 import SwiftUI
 import StoreKit
 import AppTrackingTransparency
 
 @main
 struct PokerBlindsApp: App {
-    
     var body: some Scene {
         WindowGroup {
             HomeView()
